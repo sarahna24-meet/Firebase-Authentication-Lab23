f06aa1d37fc4820f5614ee24d7e79baa6227fb5a
sarahna24-meet <139690083+sarahna24-meet@users.noreply.github.com>
2023-07-24 12:14:51 +0300

    not done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config: {
+    
+  "apiKey": "AIzaSyDuA4tdxzaTfwRrqgbxRbcvOY-_mS8fWX4",
+  "authDomain": "cs-authentication-lab.firebaseapp.com",
+  "projectId": "cs-authentication-lab",
+  "storageBucket": "cs-authentication-lab.appspot.com",
+  "messagingSenderId": "658586384685",
+  "appId": "1:658586384685:web:7f77836e36e3d54be93ba4",
+  "measurementId": "G-9QKHQWV7TB"
+  "databaseURL" : ""
+
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
